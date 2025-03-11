d311598847fe54d3df174d6b31b69dce03cf2d47
RahdainAliKhan <khanrahdainali@gmail.com>
2025-03-11 22:49:11 +0500

    EscapeSequences

---
+++ escapesequence.py
@@ -0,0 +1,13 @@
+
+'''
+In this file I learned about the escape sequence
+'''
+
+# print("\nrahdain\nali\nkhan")
+# print("\nrahdain\nali\nkhan")
+# print("\nto\nin\last")
+
+# name=input("Enter your name\n")
+# print("my name is "+name)
+
+
